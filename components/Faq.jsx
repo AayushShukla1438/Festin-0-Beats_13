58e97d87c5e08130b47fbc4c85ead2396115f93a
prateek tiwari <tiwariprateek1976@gmail.com>
2024-09-23 00:54:02 +0530

    changes

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -16,7 +16,7 @@ export default function Guidelines() {
 
     return (
         <div className="bg-black text-white p-6 md:p-8 rounded-lg mt-4">
-            <h2 className="text-4xl md:text-5xl font-bold mb-4">Genral Guidelines</h2>
+            <h2 className="text-4xl md:text-5xl font-bold mb-4">General Guidelines</h2>
             <ul className="list-disc pl-6 space-y-4">
                 {guidelines.map((guideline, index) => (
                     <li key={index} className="flex items-start space-x-3 font-semibold text-lg">
